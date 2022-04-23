8aaef39e37434e1ab1eace9af0a424e5d5ffe161
ana.aparicio <ana.aparicio@frenetic.ai>
2022-04-23 16:10:46 +0200

    add cypress and first login test

---
+++ cypress/support/commands.js
@@ -0,0 +1,32 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('https://auth.genial.ly/login')
+    cy.get('.ot-sdk-row').click()
+    cy.get('#onetrust-accept-btn-handler').click()
+    cy.get('[data-cy=loginEmailInput]').type(email)
+    cy.get('[data-cy=loginPasswordInput]').type(password)
+    cy.get('[data-cy=loginSubmitButton]').click()
+})
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
